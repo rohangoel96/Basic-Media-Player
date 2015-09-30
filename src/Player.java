907ff28f903ec56d1e3d5939be3a0194dde532f7
Rohan Goel <rohangoel0296@gmail.com>
2015-09-30 23:16:47 +0530

    Added Controls

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,8 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
@@ -7,7 +11,10 @@ import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 public class Player {
 
 	    private final JFrame frame;
-
+	    private final JButton rewindButton;
+	    private final JButton pauseButton;
+	    private final JButton forwardButton;
+	    
 	    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
 
 	    public static void main(final String[] args) {
@@ -22,12 +29,28 @@ public class Player {
 	    }
 
 	    public Player(String[] args) {
-	        frame = new JFrame("Rohan's Media Player");
+	    	JPanel contentPane = new JPanel();
+	    	contentPane.setLayout(new BorderLayout());
+
+	    	mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+	    	contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
+
+	    	JPanel controlsPane = new JPanel();
+	    	pauseButton = new JButton("Pause");
+	    	controlsPane.add(pauseButton);
+	    	rewindButton = new JButton("Rewind");
+	    	controlsPane.add(rewindButton);
+	    	forwardButton = new JButton("Forward");
+	    	controlsPane.add(forwardButton);
+	    	contentPane.add(controlsPane, BorderLayout.SOUTH);
+	    	
+	    	frame = new JFrame("Rohan's Media Player");
 	        frame.setBounds(100, 100, 600, 400);
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
-	        frame.setContentPane(mediaPlayerComponent);
-	        frame.setVisible(true);
+	    	frame.setContentPane(contentPane);
+	    	frame.setVisible(true);
+	    	
 	        mediaPlayerComponent.getMediaPlayer().playMedia(args[0]);
+
 	    }
 	}
